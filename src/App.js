8ce270b5cad622e578323899d527b74c608fda1a
Rachit Patel <rachit@mobiuso.com>
2024-06-10 19:45:32 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import EntryPoint from './Components/EntryPoint/EntryPoint';
+import Header from './Components/Header/Header';
 
 function App() {
   
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+export default function Header() {
+  return (
+    <>
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <Row>
+                <Col xs="auto">
+                    <Form.Control
+                    type="text"
+                    placeholder="Search"
+                    className=" mr-sm-2"
+                    />
+                </Col>
+                <Col xs="auto">
+                    <Button type="submit">Submit</Button>
+                </Col>
+            </Row>
+        </Form>
+        </Container>
+      </Navbar>        
+    </>
+  )
+}
